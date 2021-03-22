b6807c34ab834b020dcc402704d2dae0c3b19ba9
Mohamad Zaki Mustafa <mohamadzakimustafa@gmail.com>
2021-03-22 14:31:31 +0800

    Update web.php

--- direktori/routes/web.php
+++ direktori/routes/web.php
@@ -21,7 +21,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware(['auth'])->group(function () {
+Route::middleware(['auth', 'verified'])->group(function () {
 
     Route::get('/', function (Request $request) {
 
